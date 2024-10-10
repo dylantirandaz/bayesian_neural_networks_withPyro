01e9937bb8ec149949a933c8f9ae9ad1dcd43f07
Dylan Tirandaz <dylantirandaz@Dylans-MacBook-Air.local>
2024-10-10 09:26:43 -0500

    update

--- gaussian.py
+++ gaussian.py
@@ -2,6 +2,8 @@ import pyro
 import pyro.distributions as dist
 from pyro.nn import PyroModule, PyroSample
 import torch.nn as nn
+from pyro.infer import MCMC, NUTS
+
 
 class MyFirstBNN(PyroModule):
     def __init__(self, in_dim=1, out_dim=1, hid_dim=5, prior_scale=10.1):
@@ -33,3 +35,16 @@ class MyFirstBNN(PyroModule):
         with pyro.plate("data", x.shape[0]):
             obs = pyro.sample("obs", dist.Normal(mu, sigma*sigma), obs=y)
         return mu
+
+
+
+#markov chain monte carlo sampler
+
+model = MyFirstBNN()
+
+#set random pyro seed
+pyro.set_rng_seed(42)
+
+# Define Hamiltonian Monte Carlo (HMC) kernel
+# NUTS = "No-U-Turn Sampler" (https://arxiv.org/abs/1111.4246), gives HMC an adaptive step size
+nuts_kernel = NUTS(model, jit_compile=False)  # jit_compile=True is faster but requires PyTorch 1.6+
